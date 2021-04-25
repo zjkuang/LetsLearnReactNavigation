5df74023611eec17c4c9f33f14b82ff5ea3f538a
Zhengqian Kuang <dev.kuang@gmail.com>
2021-04-24 20:58:32 -0600

    improvements

--- components/home-screen/index.tsx
+++ components/home-screen/index.tsx
@@ -5,17 +5,14 @@ import {useNavigation} from '@react-navigation/native';
 import {RootStackNavigationProp} from '../root';
 import {QuickTestButton} from '../widgets';
 
-export type HomeScreenProps = {
-  headerTitle?: string;
-};
-export const HomeScreen = (props: HomeScreenProps) => {
+export const HomeScreen = () => {
   const stackNavigation = useNavigation<RootStackNavigationProp>();
 
   React.useLayoutEffect(() => {
     stackNavigation.setOptions({
       headerTitle: 'Home',
     });
-  }, [props.headerTitle, stackNavigation]);
+  }, [stackNavigation]);
 
   const onDetailsButtonPressed = React.useCallback(() => {
     stackNavigation.push('HomeDetails', {generation: 1});
