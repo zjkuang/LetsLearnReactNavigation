00854aeff14df4df56c9310aec6dd24ca47ab631
Zhengqian Kuang <dev.kuang@gmail.com>
2021-05-13 21:16:46 -0600

    fix a warning

--- ios/LetsLearnReactNavigation/AppDelegate.m
+++ ios/LetsLearnReactNavigation/AppDelegate.m
@@ -32,6 +32,9 @@ static void InitializeFlipper(UIApplication *application) {
 #endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+#if RCT_DEV
+  [bridge moduleForClass:[RCTDevLoadingView class]];
+#endif
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"LetsLearnReactNavigation"
                                             initialProperties:nil];
