9763a8393b9d859161f6a86d60844da6f4b2b0cb
Zhengqian Kuang <dev.kuang@gmail.com>
2021-05-20 20:39:31 -0600

    ignore warning 'If you want to use Reanimated 2 then...'

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import {LogBox} from 'react-native';
 import {RootView} from './components/root';
+
+LogBox.ignoreLogs(['If you want to use Reanimated 2 then']);
 
 const App = () => {
   return (
